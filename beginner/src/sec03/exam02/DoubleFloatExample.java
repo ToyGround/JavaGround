23f7c3c883363bca2fad28b1fa42865fa76d778a
baekCode <baekcode@gmail.com>
2020-12-15 23:06:09 +0900

    feat : double, float 차이 : 정밀도 차이

--- beginner/src/sec03/exam02/DoubleFloatExample.java
+++ beginner/src/sec03/exam02/DoubleFloatExample.java
@@ -7,8 +7,15 @@ public class DoubleFloatExample {
         float var2 = 3.14f; // long 타입처럼 값의 뒤에 소문자 f
         float var3 = 3E6F; // 또는 대문자 F 선언한다.
 
+        // double vs float : 차이? 정밀도 차이!
+        double var4 = 0.1234567890123456789;
+        float var5 = 0.1234567890123456789f;
+
         System.out.println(var1);
         System.out.println(var2);
         System.out.println(var3);
+
+        System.out.println(var4);
+        System.out.println(var5);
     }
 }
