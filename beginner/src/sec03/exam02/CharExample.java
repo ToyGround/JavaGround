18d91abba7e055bd5ec958ace3692882b5e26623
baekCode <baekcode@gmail.com>
2020-12-15 22:45:56 +0900

    feat : Char Type 유니코드, 유니코드를 저장할 땐 int변수로 저장하여 변환

---
+++ beginner/src/sec03/exam02/CharExample.java
@@ -0,0 +1,25 @@
+package sec03.exam02;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char var1 = 'A';
+        char var2 = 'B';
+        char var3 = '가';
+        char var4 = '다';
+        int let1 = var1;
+        int let2 = var2;
+        int let3 = var3;
+        int let4 = var4;
+        // char var5 = "A"; <-- error 쌍따옴표로 String Type 이다.
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+
+        System.out.println(let1);
+        System.out.println(let2);
+        System.out.println(let3);
+        System.out.println(let4);
+    }
+}
