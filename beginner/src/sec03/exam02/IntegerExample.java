55ab791dba147949dca7fb6b23e2100b1f6271f4
baekCode <baekcode@gmail.com>
2020-12-15 22:30:04 +0900

    feat : Integer 변수에 2진,8진, 16진수 입력시 변환

---
+++ beginner/src/sec03/exam02/IntegerExample.java
@@ -0,0 +1,15 @@
+package sec03.exam02;
+
+public class IntegerExample {
+    public static void main(String[] args) {
+        int var1 = 0b1011; // 2
+        int var2 = 0206; // 8
+        int var3 = 206; // 10
+        int var4 = 0xB3; // 16
+
+        System.out.println("var 1 : " + var1);
+        System.out.println("var 2 : " + var2);
+        System.out.println("var 3 : " + var3);
+        System.out.println("var 4 : " + var4);
+    }
+}
