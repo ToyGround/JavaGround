5ee66ade732200a4bfafeaf87890809014975889
baekCode <baekcode@gmail.com>
2020-12-21 23:39:18 +0900

    feat : 다중 파라미터 메소드

--- beginner/src/sec03/exam03/MethodExample.java
+++ beginner/src/sec03/exam03/MethodExample.java
@@ -4,12 +4,21 @@ public class MethodExample {
     public static void main(String[] args) {
         int n = 3;
         int a = square(n);
+        int b = sum(n, a);
 
-        System.out.printf("%d의 제곱은 %d",n,a);
+        System.out.printf("%d의 제곱은 %d \n", n, a);
+        System.out.printf("%d와 %d의 합은 %d", n, a, b);
     }
+
     public static int square(int num) {
         int result;
         result = num * num;
+        return result;
+    }
+
+    public static int sum(int a, int b) {
+        int result;
+        result = a + b;
         return result;
     }
 }
